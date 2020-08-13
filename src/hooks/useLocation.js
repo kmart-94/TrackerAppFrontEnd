a8b79ef66f5f9366195aa0d24d9d3b3a3d6d2b71
Kristian Martinez <kmartman94@gmail.com>
2020-08-12 21:26:39 -0700

    Moved helper function to useEffect and changed subscriber

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,40 +3,41 @@ import {Accuracy, requestPermissionsAsync, watchPositionAsync} from 'expo-locati
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
-
-  async function startWatching() {
-    //only called once per page render
-    try {
-      const response = await requestPermissionsAsync();
-      if (response.status === "granted") {
-        const sub = await watchPositionAsync({
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000, //once every second
-          //distanceInterval: 0 //once every ten meters
-        }, callback);
-        setSubscriber(sub);
-      }
-      else {
-        setErr('err');
-      }
-    } catch (err) {
-      setErr(err);
-    }
-  }
 
   useEffect(() => {
+    let subscriber = null;
+    async function startWatching() {
+      //only called once per page render
+      try {
+        const response = await requestPermissionsAsync();
+        if (response.status === "granted") {
+          subscriber = await watchPositionAsync({
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000, //once every second
+            //distanceInterval: 0 //once every ten meters
+          }, callback);
+        }
+        else {
+          setErr('err');
+        }
+      } catch (err) {
+        setErr(err);
+      }
+    }
+
     if (shouldTrack) {
       startWatching();
     } else {
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+        subscriber = null;
+      }
     }
 
     return () => {
       if (subscriber) {
         subscriber.remove();
-        setSubscriber(null);
+        subscriber = null;
       }
     };
   }, [shouldTrack, callback]);
